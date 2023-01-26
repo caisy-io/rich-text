e836a361f1c677b83773c176cc487f9efa18d98e
Nicolas Shiken <nicolasshiken@gmail.com>
2023-01-26 10:07:23 -0300

    re-created rich-text-vue-renderer package

---
+++ packages/rich-text-vue-renderer/src/nodes/BlockQuote.vue
@@ -0,0 +1,19 @@
+<script>
+import { attrsToStyle } from "../transformer/attrsToStyle";
+
+export default {
+  name: "BlockQuote",
+  props: {
+    node: { attrs: Object },
+  },
+  methods: {
+    attrsToStyle,
+  },
+};
+</script>
+
+<template>
+  <blockquote :style="attrsToStyle(node.attrs)">
+    <slot />
+  </blockquote>
+</template>
--- packages/rich-text-vue-renderer/src/nodes/HardBreak.vue
+++ packages/rich-text-vue-renderer/src/nodes/HardBreak.vue
@@ -7,4 +7,3 @@ export default {
 <template>
   <br />
 </template>
-
