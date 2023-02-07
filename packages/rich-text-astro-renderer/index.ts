51455c15176b71fba4f14f09c9832935b1538cb4
Moritz Gruber <m.gruber@cyclus.digital>
2023-02-07 10:15:39 +0100

    chore: added richtext for astro
    chore: added richtext for astro

---
+++ packages/rich-text-astro-renderer/index.ts
@@ -0,0 +1,7 @@
+// Do not write code directly here, instead use the `src` folder!
+// Then, use this file to export everything you want your user to access.
+
+import RichTextRenderer from './src/RichTextRenderer.astro';
+
+export {RichTextRenderer};
+export default RichTextRenderer;
